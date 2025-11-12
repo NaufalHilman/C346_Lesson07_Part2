7a155cbc2dd16d15d21cdee4e5d450b8a85e505c
NaufalHilman <24043273@myrp.edu.sg>
2025-11-12 10:23:23 +0800

    Activity 3

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.parent}>
+            <View style={[styles.child, { backgroundColor: "powderblue" }]}>
+                <Text>Child One</Text>
+            </View>
+            <View style={[styles.child, { backgroundColor: "skyblue" }]}>
+                <Text>Child Two</Text>
+            </View>
+            <View style={[styles.child, { backgroundColor: "steelblue" }]}>
+                <Text>Child Three</Text>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    parent: {
+        flex: 1,
+        backgroundColor: "#F5FCFF",
+        borderColor: "#0099AA",
+        borderWidth: 5,
+
+        // justifyContent: 'flex-start',
+        // justifyContent: 'flex-end',
+        // justifyContent: 'space-between',
+        // justifyContent: 'space-around',
+        // justifyContent: 'center',
+    },
+    child: {
+        borderWidth: 2,
+        justifyContent: "center",
+        alignItems: "center",
+        height: 80,
+    },
 });
